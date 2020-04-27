e28ca7edb36c0f796776717fed3014c22ed92906
Franck Michel <frmichel7@yahoo.fr>
2020-04-27 14:49:52 +0200

    Fix date datatype issue + update to CORD19 v7

--- src/xR2RML/run_xr2rml_all.sh
+++ src/xR2RML/run_xr2rml_all.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Author: Franck MICHEL, University Cote d'Azur, CNRS, Inria
 
-dataset=dataset-1-0
+dataset=dataset-1-1
+
 
 # Generate articles metadata
 ./run_xr2rml_metadata.sh   $dataset cord19_metadata sha   xr2rml_metadata_sha_tpl.ttl
@@ -15,5 +16,6 @@ dataset=dataset-1-0
 ./run_xr2rml_annotation.sh $dataset abstract  spotlight_light     xr2rml_spotlight_tpl.ttl
 
 # Generate annotations Entity-fishing
-./run_xr2rml_annotation.sh $dataset title     entityfishing_light xr2rml_entityfishing_tpl.ttl
-./run_xr2rml_annotation.sh $dataset abstract  entityfishing_light xr2rml_entityfishing_tpl.ttl
+./run_xr2rml_annotation.sh $dataset title     entityfishing_abstract xr2rml_entityfishing_tpl.ttl
+./run_xr2rml_annotation.sh $dataset abstract  entityfishing_abstract xr2rml_entityfishing_tpl.ttl
+./run_xr2rml_annotation.sh $dataset body_text entityfishing_abody    xr2rml_entityfishing_tpl.ttl
