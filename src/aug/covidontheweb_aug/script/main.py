2cab1723d540234ebe2d8b4b5ba99667684d9fc3
Inexpugnable <Inexpugnable@hotmail.fr>
2020-06-26 14:04:32 +0200

    Correction of the URI used

--- src/aug/covidontheweb_aug/script/main.py
+++ src/aug/covidontheweb_aug/script/main.py
@@ -20,12 +20,12 @@ if __name__ == '__main__':
     print("Generation of links associated with PICO elements.")
     umls_cui = kg.get_pico_codes()
     links_pico = kg.get_links_pico(umls_cui)
-    graph_sameas = rdf_translator.generate_closematch_relations(links_pico, rdf_translator.wikidata_uri, g)
+    graph_sameas = rdf_translator.generate_closematch_relations(links_pico, rdf_translator.umls_uri, g)
     Output.save_rdf(graph_sameas, Config.corpus_annotated + "/pico_links.nt")
 
     # Retrieve the links associated with the DBpedia entities
     print("Generation of links associated with DBpedia entities.")
     entities_dbpedia = kg.get_dbpedia_entities()
     links_dbpedia = kg.get_links_dbpedia(entities_dbpedia)
-    graph_sameas = rdf_translator.generate_closematch_relations(links_dbpedia, rdf_translator.wikidata_uri, g)
+    graph_sameas = rdf_translator.generate_closematch_relations(links_dbpedia, rdf_translator.dbpedia_uri, g)
     Output.save_rdf(graph_sameas, Config.corpus_annotated + "/dbpedia_links.nt")
